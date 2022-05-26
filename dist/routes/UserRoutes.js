6a67caba875fbd05153b4f504c181ad7566f0124
IgorGiuliano <igor.andrade07@gmail.com>
2022-05-26 14:43:27 -0300

    make it

--- dist/routes/UserRoutes.js
+++ dist/routes/UserRoutes.js
@@ -8,6 +8,7 @@ const UpdatePasswordController_1 = require("../controllers/users/UpdatePasswordC
 const UpdateUserController_1 = require("../controllers/users/UpdateUserController");
 const DeleteController_1 = require("../controllers/users/DeleteController");
 const ensureAuthenticated_1 = require("../middlewares/ensureAuthenticated");
+const GetUserDataController_1 = require("../controllers/users/GetUserDataController");
 const userRoutes = (0, express_1.Router)();
 exports.userRoutes = userRoutes;
 userRoutes.post('/login', new LoginController_1.LoginController().handle);
@@ -15,3 +16,4 @@ userRoutes.post('/register-user', new RegisterController_1.RegisterController().
 userRoutes.post('/update-password', ensureAuthenticated_1.ensureAuthenticated, new UpdatePasswordController_1.UpdatePasswordController().handle);
 userRoutes.post('/update-user', ensureAuthenticated_1.ensureAuthenticated, new UpdateUserController_1.UpdateUserController().handle);
 userRoutes.post('/delete-user', ensureAuthenticated_1.ensureAuthenticated, new DeleteController_1.DeleteController().handle);
+userRoutes.post('/get-user-data', ensureAuthenticated_1.ensureAuthenticated, new GetUserDataController_1.GetUserDataControler().handle);
